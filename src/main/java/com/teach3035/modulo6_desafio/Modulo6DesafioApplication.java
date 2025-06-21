776ddb98559e5c7685d2bebf2de78f7d5d8fca8d
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-06-21 11:58:15 -0300

    first commit

---
+++ src/main/java/com/teach3035/modulo6_desafio/Modulo6DesafioApplication.java
@@ -0,0 +1,13 @@
+package com.teach3035.modulo6_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo6DesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo6DesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teach3035/modulo6_desafio/Modulo6DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teach3035.modulo6_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo6DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
