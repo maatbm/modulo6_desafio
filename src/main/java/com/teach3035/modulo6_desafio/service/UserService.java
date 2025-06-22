6c8df93d12cea1d8ba05399184a313a7e3cbd670
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-06-22 14:51:34 -0300

    add: password encoder

--- src/main/java/com/teach3035/modulo6_desafio/service/UserService.java
+++ src/main/java/com/teach3035/modulo6_desafio/service/UserService.java
@@ -3,17 +3,25 @@ package com.teach3035.modulo6_desafio.service;
 import com.teach3035.modulo6_desafio.DTO.req.RegisterUserReqDTO;
 import com.teach3035.modulo6_desafio.DTO.res.RegisterUserResDTO;
 import com.teach3035.modulo6_desafio.exception.UserAlredyExistsExcpetion;
+import com.teach3035.modulo6_desafio.model.UserModel;
 import com.teach3035.modulo6_desafio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public RegisterUserResDTO registerUser(RegisterUserReqDTO user){
         if(userRepository.existsByUsername(user.getUsername()))
             throw new UserAlredyExistsExcpetion("User already exists with username: " + user.getUsername());
+        String encryptedPassword = passwordEncoder.encode(user.getPassword());
+        UserModel userModel = new UserModel(user.getUsername(), encryptedPassword);
+        userRepository.save(userModel);
+        return new RegisterUserResDTO(userModel.getId(), userModel.getUsername());
     }
 }
