985f308c4c70a5f613b1e44a5d3b20459638f2a5
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-06-22 15:47:04 -0300

    add: geração de token JWT

---
+++ src/main/java/com/teach3035/modulo6_desafio/service/TokenService.java
@@ -0,0 +1,31 @@
+package com.teach3035.modulo6_desafio.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.teach3035.modulo6_desafio.DTO.res.LoginResDTO;
+import com.teach3035.modulo6_desafio.model.UserModel;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    public LoginResDTO genereteToken(UserModel user){
+        String secret = "meu_secret_não_seguro";
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        Instant expirationDate = this.getExpirationDate();
+        String token = JWT.create()
+                .withIssuer("modulo6_Desafio")
+                .withSubject(user.getUsername())
+                .withExpiresAt(expirationDate)
+                .sign(algorithm);
+        return  new LoginResDTO("Bearer ", token, expirationDate.toEpochMilli());
+    }
+
+    private Instant getExpirationDate() {
+        return LocalDateTime.now().plusHours(3).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
