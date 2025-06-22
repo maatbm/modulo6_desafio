ff6065452e47adce50d8690bb692befd1b6c9c7b
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-06-22 14:05:53 -0300

    add: excepetion personalizada para usuário já existente

---
+++ src/main/java/com/teach3035/modulo6_desafio/exception/UserAlredyExistsExcpetion.java
@@ -0,0 +1,7 @@
+package com.teach3035.modulo6_desafio.exception;
+
+public class UserAlredyExistsExcpetion extends RuntimeException{
+    public UserAlredyExistsExcpetion(String message) {
+        super(message);
+    }
+}
