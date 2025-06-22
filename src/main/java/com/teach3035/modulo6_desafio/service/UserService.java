670af97357ece19f7183748c0cb1680d0816cb85
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-06-22 13:22:13 -0300

    add: user service

---
+++ src/main/java/com/teach3035/modulo6_desafio/service/UserService.java
@@ -0,0 +1,11 @@
+package com.teach3035.modulo6_desafio.service;
+
+import com.teach3035.modulo6_desafio.DTO.req.RegisterUserReqDTO;
+import com.teach3035.modulo6_desafio.DTO.res.RegisterUserResDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public RegisterUserResDTO registerUser(RegisterUserReqDTO user)
+    }
+}
