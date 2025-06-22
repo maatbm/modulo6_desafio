2312fe0c887b450f3f2ec9cc3d416f193fd68b20
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-06-22 15:47:19 -0300

    add: login

--- src/main/java/com/teach3035/modulo6_desafio/service/UserService.java
+++ src/main/java/com/teach3035/modulo6_desafio/service/UserService.java
@@ -1,13 +1,19 @@
 package com.teach3035.modulo6_desafio.service;
 
+import com.teach3035.modulo6_desafio.DTO.req.LoginReqDTO;
 import com.teach3035.modulo6_desafio.DTO.req.RegisterUserReqDTO;
+import com.teach3035.modulo6_desafio.DTO.res.LoginResDTO;
 import com.teach3035.modulo6_desafio.DTO.res.RegisterUserResDTO;
+import com.teach3035.modulo6_desafio.exception.InvalidPasswordException;
 import com.teach3035.modulo6_desafio.exception.UserAlredyExistsExcpetion;
 import com.teach3035.modulo6_desafio.model.UserModel;
 import com.teach3035.modulo6_desafio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,13 +21,26 @@ public class UserService {
     private UserRepository userRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private TokenService tokenService;
 
-    public RegisterUserResDTO registerUser(RegisterUserReqDTO user){
-        if(userRepository.existsByUsername(user.getUsername()))
+    public RegisterUserResDTO registerUser(RegisterUserReqDTO user) {
+        if (userRepository.existsByUsername(user.getUsername()))
             throw new UserAlredyExistsExcpetion("User already exists with username: " + user.getUsername());
         String encryptedPassword = passwordEncoder.encode(user.getPassword());
         UserModel userModel = new UserModel(user.getUsername(), encryptedPassword);
         userRepository.save(userModel);
         return new RegisterUserResDTO(userModel.getId(), userModel.getUsername());
+    }
+
+    public LoginResDTO loginUser(LoginReqDTO loginReqDTO) {
+        Optional<UserModel> optionalUser = userRepository.findByUsername(loginReqDTO.getUsername());
+        if (optionalUser.isEmpty())
+            throw new UsernameNotFoundException("User not found with username: " + loginReqDTO.getUsername());
+        UserModel user = optionalUser.get();
+        if (!passwordEncoder.matches(loginReqDTO.getPassword(), user.getPassword())) {
+            throw new InvalidPasswordException("Invalid password for user: " + loginReqDTO.getUsername());
+        }
+        return tokenService.genereteToken(user);
     }
 }
