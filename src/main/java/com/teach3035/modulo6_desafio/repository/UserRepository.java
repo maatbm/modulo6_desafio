13e407d80f74dcbf740d38281845399265dfa13e
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-06-22 15:46:43 -0300

    add: busca pelo username

--- src/main/java/com/teach3035/modulo6_desafio/repository/UserRepository.java
+++ src/main/java/com/teach3035/modulo6_desafio/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.teach3035.modulo6_desafio.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 
     boolean existsByUsername(String username);
+
+    Optional<UserModel> findByUsername(String username);
 }
