03461eb315d417ff8d086774feeb77aa084cbf03
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-06-24 20:44:52 -0300

    add: task controller

--- src/main/java/com/teach3035/modulo6_desafio/controller/TaskController.java
+++ src/main/java/com/teach3035/modulo6_desafio/controller/TaskController.java
@@ -1,17 +1,28 @@
 package com.teach3035.modulo6_desafio.controller;
 
+import com.teach3035.modulo6_desafio.DTO.req.GetTasksReqDTO;
+import com.teach3035.modulo6_desafio.DTO.res.GetTaskByIdResDTO;
 import com.teach3035.modulo6_desafio.DTO.res.GetTasksDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.teach3035.modulo6_desafio.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+    @Autowired
+    TaskService taskService;
+
     @GetMapping
-    public GetTasksDTO getTasks(){
-        return new GetTasksDTO(List.of());
+    public GetTasksDTO getTasks(@RequestBody GetTasksReqDTO dto, @AuthenticationPrincipal UserDetails userDetails){
+        return taskService.getTasks(dto, userDetails.getUsername());
+    }
+
+    @GetMapping("/{id}")
+    public GetTaskByIdResDTO getTaskById(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails) {
+        return taskService.getTaskById(id, userDetails.getUsername());
     }
 }
