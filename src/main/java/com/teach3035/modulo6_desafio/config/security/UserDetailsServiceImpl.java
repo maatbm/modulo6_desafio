1cbaa9f78b59876d037321381efee1c85f0098a5
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-06-25 10:26:59 -0300

    fix: indentation

--- src/main/java/com/teach3035/modulo6_desafio/config/security/UserDetailsServiceImpl.java
+++ src/main/java/com/teach3035/modulo6_desafio/config/security/UserDetailsServiceImpl.java
@@ -14,8 +14,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-      return userRepository
-              .findByUsername(username)
-              .orElseThrow(()-> new UsernameNotFoundException("User not found with username "+username));
+        return userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username " + username));
     }
 }
