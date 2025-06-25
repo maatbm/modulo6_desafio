35b674ae9ee45c362f8390d4d12fe6af3c5b3706
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-06-25 19:12:50 -0300

    fix: indentação

--- src/main/java/com/teach3035/modulo6_desafio/dto/req/CreateTaskReqDTO.java
+++ src/main/java/com/teach3035/modulo6_desafio/dto/req/CreateTaskReqDTO.java
@@ -3,10 +3,11 @@ package com.teach3035.modulo6_desafio.dto.req;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record CreateTaskReqDTO (
-    @NotBlank(message = "Title cannot be blank")
-    @Size(min = 3, max = 100, message = "Title must be between 3 and 100 characters")
-    String title,
+public record CreateTaskReqDTO(
+        @NotBlank(message = "Title cannot be blank")
+        @Size(min = 3, max = 100, message = "Title must be between 3 and 100 characters")
+        String title,
 
-    String description
-){}
+        String description
+) {
+}
