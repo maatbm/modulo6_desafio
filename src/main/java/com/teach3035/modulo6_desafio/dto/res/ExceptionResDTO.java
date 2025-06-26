b59b5e07ce01824363d0f9974409698c972fa5eb
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-06-26 11:10:20 -0300

    add: tratamento centralizado de excessões iniciado

---
+++ src/main/java/com/teach3035/modulo6_desafio/dto/res/ExceptionResDTO.java
@@ -0,0 +1,7 @@
+package com.teach3035.modulo6_desafio.dto.res;
+
+public record ExceptionResDTO(
+        String status,
+        String message
+) {
+}
---
+++ src/main/java/com/teach3035/modulo6_desafio/exception/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.teach3035.modulo6_desafio.exception;
+
+import com.teach3035.modulo6_desafio.dto.res.ExceptionResDTO;
+import com.teach3035.modulo6_desafio.exception.custom.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({UserNotFoundException.class, TaskNotFoundException.class})
+    public ExceptionResDTO resourceNotFoundExceptionHandler(ResourceNotFoundException e) {
+        return new ExceptionResDTO(HttpStatus.NOT_FOUND.name(), e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler({UserAlreadyExistsException.class})
+    public ExceptionResDTO userAlreadyExistsExceptionHandler(UserAlreadyExistsException e) {
+        return new ExceptionResDTO(HttpStatus.CONFLICT.name(), e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({InvalidStatusException.class})
+    public ExceptionResDTO invalidTaskStatusExceptionHandler(InvalidStatusException e) {
+        return new ExceptionResDTO(HttpStatus.BAD_REQUEST.name(), e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return errors;
+    }
+}
